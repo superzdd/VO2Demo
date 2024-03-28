1bb3a533a6ea1e712f8d7bf66969f8047d654ace
superzdd <superzdd@sina.com>
2024-03-28 15:34:13 +0800

    fix bug for http domain

--- src/composables/globalConfig.ts
+++ src/composables/globalConfig.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   appName: 'VO2 Demo',
-  httpDomain: 'https:asteroids.dev.mediasia.cn', // http://127.0.0.1:3001 https:asteroids.dev.mediasia.cn
+  httpDomain: 'https://asteroids.dev.mediasia.cn', // http://127.0.0.1:3001 https:asteroids.dev.mediasia.cn
   wsDomain: 'wss://asteroids.dev.mediasia.cn', // ws://127.0.0.1:3001 wss://asteroids.dev.mediasia.cn
   debug: true,
   // 其他配置...
